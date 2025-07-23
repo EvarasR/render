4b54f888b2bdad7393939bd5c6737fccc10ef07a
EvarasR <elkinvaras10@gmail.com>
2025-07-22 21:43:04 -0500

    Fix: add STATIC_ROOT setting for Render deploy

--- projectPis/settings.py
+++ projectPis/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ENLACE A TABLA USUARIOS
 AUTHENTICATION_BACKENDS = ['appPis.auth_backends.UsuarioBackend']
 
