9ee1074907bf2ed3e3add7b92ae8c6c8564d89a3
EvarasR <elkinvaras10@gmail.com>
2025-07-23 22:18:24 -0500

    Fix: usar DATABASE_URL de entorno para Render

--- projectPis/settings.py
+++ projectPis/settings.py
@@ -76,10 +76,7 @@ WSGI_APPLICATION = 'projectPis.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
